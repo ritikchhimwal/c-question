adc5a1d8a1bf1d54fe2011f6fcd11e41cb713917
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-03-20 12:19:21 +0530

    Update DDA.c

--- DDA.c
+++ DDA.c
@@ -1,27 +1,36 @@
-#include<stdio.h>
-void ddaline(int x1,int y1,int x2,int y2){
-  int dx=x2-x1;
-  int dy=y2-y1;
-
-  int steps = dx>dy ? dx:dy;
-
-  float xinc = (float)dx/steps;
-  float yinc = (float)dy/steps;
-
-  float x=x1;
-  float y=y1;
-
-  printf("pixel cooardinate");
-  for(int i=0;i<=steps;i++){
-    printf("(%0.2f,%0.2f)/n",x,y);
-    x+=xinc;
-    y+=yinc;
-  }
-}
-
-int main(){
-  int x1,y1,x2,y2;
-  x1=x2=x3=x4=50;
-  ddaline(x1,x2,x3,x4);
-  return 0;
-}
+#include<graphics.h>  
+#include<conio.h>  
+#include<stdio.h>  
+void main()  
+{  
+    intgd = DETECT ,gm, i;  
+    float x, y,dx,dy,steps;  
+    int x0, x1, y0, y1;  
+    initgraph(&gd, &gm, "C:\\TC\\BGI");  
+    setbkcolor(WHITE);  
+    x0 = 100 , y0 = 200, x1 = 500, y1 = 300;  
+    dx = (float)(x1 - x0);  
+    dy = (float)(y1 - y0);  
+    if(dx>=dy)  
+           {  
+        steps = dx;  
+    }  
+    else  
+           {  
+        steps = dy;  
+    }  
+    dx = dx/steps;  
+    dy = dy/steps;  
+    x = x0;  
+    y = y0;  
+    i = 1;  
+    while(i<= steps)  
+    {  
+        putpixel(x, y, RED);  
+        x += dx;  
+        y += dy;  
+        i=i+1;  
+    }  
+    getch();  
+    closegraph();  
+}  
